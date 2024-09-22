fd351cf94c4a3d1f751cb6e941c6278d186049e7
Amirhossein Adibinia <amir.adibinia@gmail.com>
2024-09-22 18:07:39 +0330

    feat: add new methods to VM

--- VM.py
+++ VM.py
@@ -13,10 +13,11 @@ class VM:
         'revertSnap': r' -T ws revertToSnapshot ',
     }
 
-    def __init__(self, vmrun_path: str, vmx_path: str, initial_snapshot_name: str):
+    initial_snapshot_name: str
+
+    def __init__(self, vmrun_path: str, vmx_path: str):
         self.vmrun = vmrun_path
         self.vmx = vmx_path
-        self.initial_snapshot_name = initial_snapshot_name
 
     def start_vm(self):
         cmd = self.vmrun + self.operation_map['start'] + self.vmx
@@ -54,6 +55,33 @@ class VM:
         shutil.copy(memory_file, dest_file)
         print("Memory dump copied successfully.")
 
+    def capture_initial_snapshot(self, snapshot_name, destination_path):
+        """
+        This function takes a snapshot of the running VM and copies the memory dump (.vmem) file
+        to the specified destination directory.
+        """
+        # Take the snapshot
+        snapshot_cmd = self.vmrun + self.operation_map['createSnap'] + self.vmx + f' "{snapshot_name}"'
+        print(f"Taking snapshot: {snapshot_cmd}")
+        subprocess.run(snapshot_cmd, shell=True)
+
+        # Assuming the .vmem file is in the same directory as the VMX file
+        vm_dir = os.path.dirname(self.vmx.replace('"', ''))  # Remove quotes to get the actual path
+        memory_file = os.path.join(vm_dir,
+                                   f'{os.path.splitext(os.path.basename(self.vmx))[0]}-Snapshot{snapshot_name}.vmem')
+
+        # Ensure the memory dump exists
+        if not os.path.exists(memory_file):
+            print(f"Error: Memory dump file {memory_file} not found!")
+            sys.exit(1)
+
+        # Copy the memory dump to the destination
+        dest_file = os.path.join(destination_path, f'{snapshot_name}.vmem')
+        print(f"Copying memory dump from {memory_file} to {dest_file}")
+        shutil.copy(memory_file, dest_file)
+        self.initial_snapshot_name = snapshot_name
+        print("Memory dump copied successfully.")
+
     def revert_to_initial_state(self):
         """
         Reverts the virtual machine to the initial snapshot.
@@ -64,6 +92,9 @@ class VM:
         subprocess.run(revert_cmd, shell=True)
 
     def revert_to_snapshot(self, snapshot_name: str):
+        revert_cmd = self.vmrun + self.operation_map['revertSnap'] + self.vmx + f' "{snapshot_name}"'
+        print(f"Reverting VM to initial snapshot: {revert_cmd}")
+        subprocess.run(revert_cmd, shell=True)
 
 
     def operate_vm(self, operation: str):
--- main.py
+++ main.py
@@ -1,43 +1,15 @@
 import os
 import subprocess
 import sys
-
-# Path to vmrun executable
-vmrun = r'"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"'  # Modify this to your vmrun path
-
-# Map the name of the VM to the .vmx file
-vmx_map = {
-    'Windows10': r'"C:\Users\Amir\Documents\Virtual Machines\Windows 10 x64\Windows 10 x64.vmx"'  # Modify this to your VM's .vmx file path
-}
-
-# Operation mappings for vmrun
-operation_map = {
-    'start': r' -T ws start ',
-    'stop': r' -T ws stop ',
-}
-
-
-def operate_vm(host, operation):
-    """
-    This function performs operations on the virtual machine like starting and stopping.
-    """
-    if host not in vmx_map:
-        print(f"Error: Host '{host}' does not exist!")
-        sys.exit(1)
-
-    if operation not in operation_map:
-        print(f"Error: Operation '{operation}' is invalid!")
-        sys.exit(1)
-
-    cmd = vmrun + operation_map[operation] + vmx_map[host]
-    print(f"Executing: {cmd}")
-    subprocess.run(cmd, shell=True)
+from dotenv import load_dotenv
+from VM import VM
 
 
 if __name__ == '__main__':
-    # Name of the VM (must be in vmx_map)
-    host = 'Windows10'  # Modify this to the name you are using in vmx_map
+    vmrun_path = os.getenv('VMRUN_PATH')
+    vmx_path = os.getenv('VMX_PATH')
+
+    vm = VM(vmrun_path, vmx_path)
 
     # Start the VM
     print("Starting the virtual machine...")
-    operate_vm(host, 'start')
